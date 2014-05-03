3b4285a79bf62c1a0876c13049fad2f2c585b765
NeilKey <neil.key@bluewin.ch>
2014-05-03 19:00:58 +0200

    Init project

---
+++ ChatClientForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ChatClient3AA
+{
+    public partial class ChatClientForm : Form
+    {
+        public ChatClientForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
